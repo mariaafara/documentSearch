5eac6086b73650b26ffe174951a5c0371079704a
mariaafara <maria-afara5@hotmail.com>
2023-02-05 10:54:56 +0100

    format test

--- tests/test_searcher.py
+++ tests/test_searcher.py
@@ -14,25 +14,43 @@ def test_searcher():
     searcher = Searcher(processor, in_memory_index_store)
 
     assert (
-            DeepDiff(
-                searcher.search(query_terms=["token1 token7", "token2"]),
-                [
-                    ("id_1", ["token1", "token2"]),
-                    ("id_2", ["token2"]),
-                    ("id_3", ["token2"]),
-                ],
-                ignore_order=True,
-            )
-            == {}
+        DeepDiff(
+            searcher.search(query_terms=["token1 token7", "token2"]),
+            [
+                ("id_1", ["token1", "token2"]),
+                ("id_2", ["token2"]),
+                ("id_3", ["token2"]),
+            ],
+            ignore_order=True,
+        )
+        == {}
     )
     assert (
-            DeepDiff(searcher.search(query_terms=["token1"]), [("id_1", ["token1"])], ignore_order=True) == {}
+        DeepDiff(
+            searcher.search(query_terms=["token1"]),
+            [("id_1", ["token1"])],
+            ignore_order=True,
+        )
+        == {}
     )
-    assert DeepDiff(searcher.search(query_terms=["token8"]), [], ignore_order=True, ) == {}
     assert (
-            DeepDiff(
-                searcher.search(query_terms=["token5"], ),
-                [("id_2", ["token5"]), ("id_3", ["token5"]), ],
-                ignore_order=True, )
-            == {}
+        DeepDiff(
+            searcher.search(query_terms=["token8"]),
+            [],
+            ignore_order=True,
+        )
+        == {}
+    )
+    assert (
+        DeepDiff(
+            searcher.search(
+                query_terms=["token5"],
+            ),
+            [
+                ("id_2", ["token5"]),
+                ("id_3", ["token5"]),
+            ],
+            ignore_order=True,
+        )
+        == {}
     )
