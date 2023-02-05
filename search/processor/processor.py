867bc7105f375894b4c9e1a3c83de019e297a1fc
mariaafara <maria-afara5@hotmail.com>
2023-02-05 19:02:10 +0100

    add docstrings to processor

--- search/processor/processor.py
+++ search/processor/processor.py
@@ -17,34 +17,39 @@ class Processor:
 
     @staticmethod
     def _lowercase(text) -> str:
-        # convert the document to lowercase
+        """Lowercase text input."""
         return text.lower()
 
     @staticmethod
     def _punctuation_filter(text) -> str:
+        """Remove punctuation from text input."""
         return text.translate(str.maketrans("", "", string.punctuation))
 
     @staticmethod
     def _new_lines_filter(text) -> str:
+        """Remove new lines from text input."""
         return text.replace("\n", " ")
 
     @staticmethod
     def _tokenize(text) -> List[str]:
+        """Remove tokenize text input."""
         return text.split()
         # return nltk.word_tokenize(document)
 
     @staticmethod
     def _stopwords_filter(tokens: List[str]) -> List[str]:
+        """Remove stop words from input tokens."""
         stop_words = set(stopwords.words("english"))
         return [token for token in tokens if token.lower() not in stop_words]
 
     @staticmethod
     def _lemmatize(tokens: List[str]) -> List[str]:
+        """Lemmatize input tokens."""
         return [lemmatizer.lemmatize(token) for token in tokens]
 
     @staticmethod
     def _get_ngrams(tokens: List[str], n):
-        """Generate n-grams."""
+        """Generate n-grams from input token."""
         ngrams = set()
         for i in range(1, n + 1):
             ngrams.update(nltk.ngrams(tokens, i))
@@ -59,6 +64,7 @@ class Processor:
         return None
 
     def preprocess(self, text: str) -> (List[Tuple[str]], Union[None, List[float]]):
+        """Preprocess input text."""
         # Remove punctuations
         text = Processor._punctuation_filter(text)
         # Remove new lines
