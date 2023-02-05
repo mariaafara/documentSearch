ecc6b6d29fe10f98cf888262aee8000902c1a34a
mariaafara <maria-afara5@hotmail.com>
2023-02-05 12:56:20 +0100

    Change inmemory idex store data structure

--- search/index_store/in_memory.py
+++ search/index_store/in_memory.py
@@ -13,17 +13,19 @@ class InMemoryIndexStore(IndexStore):
 
     def __init__(self):
         super(IndexStore).__init__()
-        self.indices_dict = defaultdict(set)
+        # self.indices_dict = defaultdict(set)
+        self.indices_dict = defaultdict(
+            lambda: defaultdict(set))  # nested defaultdict {ngram: { 'embedding' :{[]}, 'doc_ids': {[]}}}
 
     def add_doc(self, doc_id, tokens) -> None:
         for token in tokens:
-            self.indices_dict[token].add(doc_id)
+            self.indices_dict[token]["doc_ids"].add(doc_id)
 
     def get_docs(self, tokens: List[str]) -> List[Tuple[str, List[str]]]:
         """return List[Tuple[str, List[str]]]: [(doc_id, [tokens matched in doc]),etc]"""
         result_dict = defaultdict(list)
         for token in tokens:
-            docs_ids = self.indices_dict[token]
+            docs_ids = self.indices_dict[token]["doc_ids"]
             for doc_id in docs_ids:
                 result_dict[doc_id].append(token)
         return list(result_dict.items())
